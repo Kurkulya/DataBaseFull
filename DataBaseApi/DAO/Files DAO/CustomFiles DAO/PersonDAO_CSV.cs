b05badabfd0f7dc4b77201d31c8f8630713d34b3
Kulinich Vlad <kulinich_v@hotmail.com>
2017-10-30 01:05:41 +0200

    Added manual csv phones

--- DataBaseApi/DAO/Files DAO/CustomFiles DAO/PersonDAO_CSV.cs
+++ DataBaseApi/DAO/Files DAO/CustomFiles DAO/PersonDAO_CSV.cs
@@ -34,7 +34,7 @@ namespace DataBaseApi
         protected override void Write(List<Person> people)
         {
             StreamWriter file = new StreamWriter(path);
-            file.WriteLine("Id, Fn, Ln, Age");
+            file.WriteLine("Id, FirstName, LastName, Age, Phones");
             foreach (Person person in people)
             {
                 file.WriteLine(ToCSV(person));
@@ -49,18 +49,51 @@ namespace DataBaseApi
             str += person.Id + ", ";
             str += person.FirstName + ", ";
             str += person.LastName + ", ";
-            str += person.Age;
+            str += person.Age + ", ";
+            str += "\"[";
+            foreach (Phone phone in person.Phones)
+            {
+                str += "{";
+                str += $"Id: {phone.Id},";
+                str += $"Number: {phone.Number},";
+                str += $"PersonId: {phone.PersonId}";
+                str += "},";
+            }
+            str.TrimEnd(',');
+            str += "]\"";
             return str;
         }
 
         private Person FromCSV(string csv_string)
         {
             Person person = new Person();
-            string[] args = csv_string.Split(',');
+            Phone phone = new Phone();
+            string[] phones = csv_string.Split('[')[1].Split('{','}',',');
+            string[] args = csv_string.Split('[')[0].Split(',');
+
             person.Id = Int32.Parse(args[0].Trim());
             person.FirstName = args[1].Trim();
             person.LastName = args[2].Trim();
             person.Age = Int32.Parse(args[3].Trim());
+
+            foreach (string str in phones)
+            {
+                if (str != "" && str != "]\"")
+                {
+                    string[] keyVal = str.Split(':');
+                    keyVal = keyVal.Select(x => x.Trim()).ToArray();
+                    switch (keyVal[0])
+                    {
+                        case "Id":
+                            phone = new Phone();
+                            phone.Id = Convert.ToInt32(keyVal[1]); break;
+                        case "Number": phone.Number = keyVal[1]; break;
+                        case "PersonId":
+                            phone.PersonId = Convert.ToInt32(keyVal[1]);
+                            person.Phones.Add(phone); break;
+                    }
+                }
+            }
             return person;
         }
     }
